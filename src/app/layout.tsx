d5889f68409aebce15c7fcfdcf104ad3014f6ac0
shormigab-collab <s.hormigab@gmail.com>
2026-09-18 14:33:15 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "./providers";
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: "Prospección · Fast Dolphin",
   description:
